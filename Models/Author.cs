13e5d2567bcd899d37f02ebb390e1021d96eab6b
j-guzik <justyna.b.guzik@gmail.com>
2021-01-16 22:05:51 +0100

    Add Models Author and Item

---
+++ Models/Author.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuseumManagement.Models
+{
+    public class Author
+    {
+        [Key]
+        public int IdAuthor { get; set; }
+
+        [Required(ErrorMessage = "Prosze podac imię autora!")]
+        [Display(Name = "Imię autora")]
+        [StringLength(30, ErrorMessage = "Imię nie może być dłuższe niż 30 znaków.")]
+        public string AuthorName { get; set; }
+
+        [Required(ErrorMessage = "Prosze podac nazwisko autora!")]
+        [Display(Name = "Nazwisko autora")]
+        [StringLength(50, ErrorMessage = "Nazwisko nie może być dłuższe niż 50 znaków.")]
+        public string AuthorSurname { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BornDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DeathDate { get; set; }
+
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MuseumManagement.Models
+{
+    public class Item
+    {
+        [Key]
+        public int IdItem { get; set; }
+
+        [Required(ErrorMessage = "Prosze podac nazwę eksponatu!")]
+        [Display(Name = "Nazwa eksponatu")]
+        [StringLength(100, ErrorMessage = "Nazwa nie może być dłuższa niż 100 znaków.")]
+        public string ItemName { get; set; }
+
+        [Display(Name = "Rok powstania")]
+        public int ItemYear { get; set; }
+    }
+}
